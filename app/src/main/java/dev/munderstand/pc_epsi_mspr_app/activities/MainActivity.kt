136db1fe9d0c1733e8a471867fd393c327e06c54
Munderstand <anchidins0@gmail.com>
2023-06-20 23:04:02 +0200

    Added Main activity and base components and API config

---
+++ app/src/main/java/dev/munderstand/pc_epsi_mspr_app/activities/MainActivity.kt
@@ -0,0 +1,68 @@
+package dev.munderstand.pc_epsi_mspr_app.activities
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
+import dev.munderstand.pc_epsi_mspr_app.R
+import dev.munderstand.pc_epsi_mspr_app.activities.common.BaseActivity
+import dev.munderstand.pc_epsi_mspr_app.fragments.*
+
+class MainActivity : BaseActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+        bottomNavigationView = findViewById(R.id.navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.tabAcceuil -> {
+                    replaceFragment(AcceuilFragment())
+                    true
+                }
+                R.id.tabMaps -> {
+                    replaceFragment(MapFragment())
+                    true
+                }
+                R.id.tabCamera -> {
+                    replaceFragment(BotanistesFragment())// Handle camera placeholder item
+                    true
+                }
+                R.id.tabPlantes -> {
+                    replaceFragment(PlantesFragment())
+                    true
+                }
+                R.id.tabAccount -> {
+                    replaceFragment(AccountFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+        // Set the default fragment
+        replaceFragment(AcceuilFragment())
+          setHeaderTxt("Menu")
+         showBack()
+          showRight()
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.content, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/dev/munderstand/pc_epsi_mspr_app/activities/common/ApiConfig.kt
@@ -0,0 +1,20 @@
+package dev.munderstand.pc_epsi_mspr_app.activities.common
+
+class ApiConfig {
+    companion object {
+        const val BASE_URL = "http://88.125.155.66:32768/api"
+        const val LOGIN_ENDPOINT = "$BASE_URL/login_check"
+        const val REGISTER_ENDPOINT = "$BASE_URL/register"
+
+        const val ACCOUNT_ENDPOINT = "$BASE_URL/main/account"
+
+
+        const val PLANTS_ENDPOINT = "$BASE_URL/plante/"
+        const val PLANT_ADD_ENDPOINT = "$BASE_URL/plante/add"
+
+        const val BOTANIST_ENDPOINT = "$BASE_URL/botaniste"
+
+
+        // Add other endpoints as needed
+    }
+}
