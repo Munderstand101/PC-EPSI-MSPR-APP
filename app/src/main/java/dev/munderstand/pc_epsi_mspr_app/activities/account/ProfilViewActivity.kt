d9ef568682f8be2f4cae0310164849efde8a11d4
Munderstand <anchidins0@gmail.com>
2023-06-20 23:06:33 +0200

    Added Settings and Account profile view and edit activity

---
+++ app/src/main/java/dev/munderstand/pc_epsi_mspr_app/activities/account/ProfilViewActivity.kt
@@ -0,0 +1,12 @@
+package dev.munderstand.pc_epsi_mspr_app.activities.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.munderstand.pc_epsi_mspr_app.R
+
+class ProfilViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_view)
+    }
+}
